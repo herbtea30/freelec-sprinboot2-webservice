deddd6228c2f8fbbbedfb856accfb252b8b90143
herbtea <herbtea30@gmail.com>
2020-08-05 00:27:08 +0900

    1. HelloController 테스트코드 작성 2. HelloResponseDto 테스트코드 작성

---
+++ src/main/java/com/herbtea/boot/springboot/Application.java
@@ -0,0 +1,11 @@
+package com.herbtea.boot.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
