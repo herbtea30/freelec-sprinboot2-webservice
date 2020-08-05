5c1f438f89ce2e1667bfbdd9a448f8cc911ace5f
herbtea <herbtea30@gmail.com>
2020-08-05 22:55:06 +0900

    1. 게시판 등록 수정 삭제 2. 테스트코드 작성 완료

---
+++ src/main/java/com/herbtea/boot/springboot/service/PostsService.java
@@ -0,0 +1,34 @@
+package com.herbtea.boot.springboot.service;
+
+import com.herbtea.boot.springboot.domain.posts.Posts;
+import com.herbtea.boot.springboot.domain.posts.PostsRepository;
+import com.herbtea.boot.springboot.web.dto.PostsResponseDto;
+import com.herbtea.boot.springboot.web.dto.PostsSaveRequestDto;
+import com.herbtea.boot.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
+    }
+}
