dbb13655408a402b0ed06af3990a8ffb915dda32
herbtea <herbtea30@gmail.com>
2020-08-06 22:05:58 +0900

    1. 게시판 화면 머스테치 활용한 화면 구성

--- src/main/java/com/herbtea/boot/springboot/service/PostsService.java
+++ src/main/java/com/herbtea/boot/springboot/service/PostsService.java
@@ -2,13 +2,16 @@ package com.herbtea.boot.springboot.service;
 
 import com.herbtea.boot.springboot.domain.posts.Posts;
 import com.herbtea.boot.springboot.domain.posts.PostsRepository;
+import com.herbtea.boot.springboot.web.dto.PostsListResponseDto;
 import com.herbtea.boot.springboot.web.dto.PostsResponseDto;
 import com.herbtea.boot.springboot.web.dto.PostsSaveRequestDto;
 import com.herbtea.boot.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -27,8 +30,21 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
+    }
+
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
---
+++ src/main/java/com/herbtea/boot/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,21 @@
+package com.herbtea.boot.springboot.web.dto;
+
+import com.herbtea.boot.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
