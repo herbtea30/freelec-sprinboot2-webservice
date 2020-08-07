95b8a123e8cb90b5a35d7be4c0212ea5420a90f3
herbtea <herbtea30@gmail.com>
2020-08-07 17:55:48 +0900

    Spring Security oauth2 Test 적용

---
+++ src/main/java/com/herbtea/boot/springboot/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.herbtea.boot.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
